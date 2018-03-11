44e630be03cea71fcc83fb9bc23c49aa92cc85da
Burçak Sayınsoy <burcak@sayinsoy.com>
2018-03-11 23:38:31 +0300

    Destroy Action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,6 +38,13 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
